6ada7eb99fb385447b7253d0da8d5a1924a56040
lfish <shineccy@aliyun.com>
2023-10-25 11:02:37 +0800

    standarlize the features

--- gen_dataset_and_test.py
+++ gen_dataset_and_test.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split  # 划分数据集与测试集
 from sklearn import metrics  # 分类结果评价函数
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import accuracy_score  # 导入评分模块
 from sklearn import svm  # 导入算法模块
 from sklearn.ensemble import (
@@ -29,10 +31,27 @@ args = parser.parse_args()
 tdata = pd.read_csv(args.trueFeatures)
 fdata = pd.read_csv(args.falseFeatures)
 
+# add labels
+tdata["is_https"] = 1
+fdata["is_https"] = 0
+
+# gen dataset
+dataset = pd.concat([tdata, fdata])
+
+# --------------------------------preprocessing
+
+# define features of number type
+number_features_name = [
+    "port",
+    "cipher",
+    "server_hello_length",
+    "server_hello_extensions_length",
+]
+
 # port as features?
 if not args.withPort:
-    tdata.drop(columns="port", inplace=True)
-    fdata.drop(columns="port", inplace=True)
+    dataset.drop(columns="port", inplace=True)
+    number_features_name.remove("port")
 
 # use single tls version
 protocol = 0
@@ -46,36 +65,44 @@ elif args.tlsVersion == "tls1.3":
     protocol = 772
 
 if protocol != 0:
-    tdata = tdata[tdata["protocol"] == protocol]
-    fdata = fdata[fdata["protocol"] == protocol]
-    tdata.drop(columns="protocol", inplace=True)
-    fdata.drop(columns="protocol", inplace=True)
+    dataset = dataset[dataset["protocol"] == protocol]
+    dataset.drop(columns="protocol", inplace=True)
+else:
+    # onehot encode
+    dataset["is_tls1.0"] = dataset["protocol"].apply(lambda x: 1 if x == 769 else 0)
+    dataset["is_tls1.1"] = dataset["protocol"].apply(lambda x: 1 if x == 770 else 0)
+    dataset["is_tls1.2"] = dataset["protocol"].apply(lambda x: 1 if x == 771 else 0)
+    dataset["is_tls1.3"] = dataset["protocol"].apply(lambda x: 1 if x == 772 else 0)
+    dataset.drop(columns="protocol", inplace=True)
 
 
-# add labels
-tdata["is_https"] = 1
-fdata["is_https"] = 0
-# gen dataset
-dataset = pd.concat([tdata, fdata])
+# standard number type features and return to dataframe
+number_features = dataset[number_features_name]
+std_transfer = StandardScaler()
+number_features = pd.DataFrame(
+    std_transfer.fit_transform(number_features), columns=number_features_name
+)
+
+dataset.drop(labels=number_features_name, axis=1, inplace=True)
+dataset.reset_index(drop=True, inplace=True)
+dataset = pd.concat([number_features, dataset], axis=1)
 
 # ---------------------training
 train = dataset.drop(["is_https"], axis=1)
 X_train, X_test, y_train, y_test = train_test_split(
-    train, dataset["is_https"], random_state=1, train_size=0.8
+    train,
+    dataset["is_https"],
+    random_state=1,
+    train_size=0.8,
+    stratify=dataset["is_https"],
 )
-
-# svm_linear = svm.SVC(C=1, kernel="linear", decision_function_shape="ovo")
-# svm_linear.fit(X_train, y_train)
-# print("SVM训练模型评分：" + str(accuracy_score(y_train, svm_linear.predict(X_train))))
-# print("SVM待测模型评分：" + str(accuracy_score(y_test, svm_linear.predict(X_test))))
 
 model = RandomForestClassifier()  # 实例化模型RandomForestClassifier
 model.fit(X_train, y_train)  # 在训练集上训练模型
 print(model)  # 输出模型RandomForestClassifier
 
-# 在测试集上测试模型
-expected = y_test  # 测试样本的期望输出
-predicted = model.predict(X_test)  # 测试样本预测
+expected = y_test
+predicted = model.predict(X_test)
 
 # 输出结果
 print(metrics.classification_report(expected, predicted))  # 输出结果，精确度、召回率、f-1分数
